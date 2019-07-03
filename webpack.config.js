8f1f0f32749c82b3760b787a6d7192e2c6740643
Na Hyung Choi <schoinh@gmail.com>
2019-07-03 09:35:24 -0700

    add Dotenv()

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/main.js',
@@ -20,7 +21,8 @@ module.exports = {
       title: 'Project Name',      // CHANGE THIS
       template: './src/index.html',
       inject: 'body'
-    })
+    }),
+    new Dotenv()
   ],
   module: {
     rules: [
